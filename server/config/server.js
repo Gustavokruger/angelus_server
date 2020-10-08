ed4805d67460afa3641ad687578ddf5605f5275e
rafaelmessiah <rafaelmessiah@gmail.com>
2020-10-08 10:14:16 -0300

    arquivo cors

--- server/config/server.js
+++ server/config/server.js
@@ -1,19 +1,26 @@
 'use strict'
 
+//Dependências do arquivo
 console.clear();
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(routes);
+app.use(cors());
 
+
+//Definir o que o servidor irá escutar
 const port = 1234;
 
+//Configurando o servidor para escutar a porta definida
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+ console.log(`Servidor rodando na porta ${port}`);
 });
